14fd9991580ba83e5bb4434d88f7d7eb500483b5
daiba <badaibou5@gmail.com>
2022-11-04 03:28:08 +0000

    my first go commit

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
 
 func main() {
-	fmt.Printf("abcdefghijklmnopqrstuvwxyz")
+	name := os.Args[0]
+	a := []rune(name)
+	for i := range a {
+		z01.PrintRune(a[i])
+	}
+	z01.PrintRune(10)
 }
