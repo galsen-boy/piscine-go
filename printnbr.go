f3207966d4148433ef89e464053d881a21fd4ba3
daiba <badaibou5@gmail.com>
2022-11-04 22:21:26 +0000

    my seventh go commit

--- printnbr.go
+++ printnbr.go
@@ -2,19 +2,19 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintNum(k int) {
+func PrintNum(num int) {
 	i := '0'
-	if k == 0 {
+	if num == 0 {
 		z01.PrintRune('0')
 	}
-	for j := 1; j <= k%10; j++ {
+	for j := 1; j <= num%10; j++ {
 		i++
 	}
-	for j := -1; j >= k%10; j-- {
+	for j := -1; j >= num%10; j-- {
 		i++
 	}
-	if k/10 != 0 {
-		PrintNbr(k / 10)
+	if num/10 != 0 {
+		PrintNum(num / 10)
 	}
 	z01.PrintRune(i)
 }
