d65511f4469c36a31934017032af6dd8bdedda57
daiba <badaibou5@gmail.com>
2022-11-04 20:29:08 +0000

    my sixth go commit

--- printcomb2.go
+++ printcomb2.go
@@ -7,12 +7,12 @@ func PrintComb2() {
 		for j := '0'; j <= '9'; i++ {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(l)
 					if i == k && l == j+1 {
-						z01.PrintRune(i)
-						z01.PrintRune(j)
-						z01.PrintRune(' ')
-						z01.PrintRune(k)
-						z01.PrintRune(l)
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
