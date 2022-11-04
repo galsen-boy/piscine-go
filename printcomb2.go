4d7a8fbb327dbea0f628f2d8d71c9570ac27fc12
daiba <badaibou5@gmail.com>
2022-11-04 20:24:19 +0000

    my sixth go commit

--- printcomb2.go
+++ printcomb2.go
@@ -2,16 +2,17 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintComb() {
-	a := '0'
-	b := '1'
-	for i := a; i <= 55; i++ {
-		for j := b; j <= 56; j++ {
-			{
-				if i < j {
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					if i != 55 {
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; i++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
+					if i == k && l == j+1 {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(l)
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
