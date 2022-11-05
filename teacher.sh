e465a2316df2b2694105d39897c6d9bdaf75424b
daiba <badaibou5@gmail.com>
2022-11-05 02:39:29 +0000

    my sixteenth commit

--- teacher.sh
+++ teacher.sh
@@ -1,6 +1,5 @@
-INTERVIEWNUMBER=$(head -n 179 streets/Buckingham_place | tail -n 1 | cut -d "#" -f2)
-echo "$INTERVIEWNUMBER"
-cat interviews/interview-"$INTERVIEWNUMBER"
-grep -A 4 L337 vehicles | grep -A 3 -B 1 Honda | grep -A 2 -B 2 Blue | grep -B 4 "Height: 6"
-cat memberships/Delta_SkyMiles memberships/Museum_of_Bash_History memberships/Terminal_City_Library | grep "$MAIN_SUSPECT" | wc -l
-echo 3873
+$MAIN_SUSPECT="Dartey Henv"
+export KEY_INTERVIEW_NUMBER=$(head -n 179 ./streets/Buckingham_Place | tail -n 1 | cut -d '#' -f 2)
+echo $KEY_INTERVIEW_NUMBER
+cat ./interviews/interview-$KEY_INTERVIEW_NUMBER
+echo $MAIN_SUSPECT
