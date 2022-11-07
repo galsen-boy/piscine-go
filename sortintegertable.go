f7c23ed6831a930e67db1313e2326facdec1251b
daiba <badaibou5@gmail.com>
2022-11-07 03:55:02 +0000

    my twenty_oneth go commit

--- sortintegertable.go
+++ sortintegertable.go
@@ -4,7 +4,7 @@ func SortIntegerTable(table []int) {
 	for i := 0; i < len(table); i++ {
 		for j := 0; j < len(table); j++ {
 			if table[i] < table[j] {
-				table[i], table[j] = table[j], table[j]
+				table[i], table[j] = table[j], table[i]
 			}
 		}
 	}
