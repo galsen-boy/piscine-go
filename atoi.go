6a583295b610e1688ec99daa630bcfae1726f92f
daiba <badaibou5@gmail.com>
2022-11-07 03:11:25 +0000

    mi twentyth go commit

--- atoi.go
+++ atoi.go
@@ -1,12 +1,29 @@
 package piscine
 
 func Atoi(s string) int {
-	num := 0
-	for _, i := range s {
-		if i == '-' || i < '0' || i > '9' {
+	if StrLen(s) == 0 {
+		return 0
+	}
+
+	s0 := s
+	if s[0] == '-' || s[0] == '+' {
+		s = s[1:]
+		if StrLen(s) < 1 {
 			return 0
 		}
-		num = num*10 + int(i) - '0'
 	}
-	return num
+
+	nm := 0
+
+	for _, i := range s {
+		if i < '0' || i > '9' {
+			return 0
+		}
+		nm = nm*10 + int(i)
+	}
+
+	if s0[0] == '-' {
+		nm *= -1
+	}
+	return nm
 }
