75c02974b3552a0447ba412296919edd3006ccb7
daiba <badaibou5@gmail.com>
2022-11-08 10:55:02 +0000

    my 22th go commit

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,20 +1,12 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 20 {
-		var a int
-		a = 1
-		if nb < 0 {
-			return 0
-		}
-		for l := 1; l <= nb; l++ {
-			a = a * l
-		}
-		return a
-	} else {
+	baye_fall := 1
+	if nb > 25 || nb < 0 {
 		return 0
 	}
-}
-
-func main() {
+	for i := 1; i <= nb; i++ {
+		baye_fall = baye_fall * i
+	}
+	return baye_fall
 }
