16c5674221ac5340618e05e41db022c05a14c801
daiba <badaibou5@gmail.com>
2022-11-08 11:17:45 +0000

    my 24th go commit

--- iterativepower.go
+++ iterativepower.go
@@ -1,12 +1,12 @@
 package piscine
 
-func RecursivePower(nb int, power int) int {
+func IterativePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
-	if power == 0 {
-		return 1
+	baye_fall := 1
+	for i := 0; i < power; i++ {
+		baye_fall = baye_fall * nb
 	}
-
-	return nb * RecursivePower(nb, power-1)
+	return baye_fall
 }
