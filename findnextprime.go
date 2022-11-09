696aa5f7345b7139fc6bb8283a5beba1b23b0840
daiba <badaibou5@gmail.com>
2022-11-09 02:15:30 +0000

    my27th go commit

--- findnextprime.go
+++ findnextprime.go
@@ -3,15 +3,18 @@ package piscine
 func FindNextPrime(nb int) int {
 	a := nb
 	b := 0
-	for i := nb; i >= 1; i-- {
-		a = nb % i
-		if a == 0 {
-			b++
+	if !(nb < 0) {
+		for i := nb; i >= 1; i-- {
+			a = nb % i
+			if a == 0 {
+				b++
+			}
+		}
+		if b == 2 {
+			return nb
+		} else {
+			return FindNextPrime(nb + 1)
 		}
 	}
-	if b == 2 {
-		return nb
-	} else {
-		return FindNextPrime(nb + 1)
-	}
+	return b
 }
