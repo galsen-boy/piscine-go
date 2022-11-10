f3eee73a88022f8d16e08863e752cb2478e5574b
daiba <badaibou5@gmail.com>
2022-11-10 05:12:57 +0000

    my 42 go commit

--- eightqueens.go
+++ eightqueens.go
@@ -1,68 +1,36 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
-var x = []rune{'1', '2', '3', '4', '5', '6', '7', '8'}
+func safe(yad int, kid int, pos [8]int) bool {
+	for i := 0; i < yad; i++ {
+		t := pos[i]
+		if t == kid || t == kid-(yad-i) || t == kid+(yad-i) {
+			return false
+		}
+	}
+	return true
+}
+
+func resoudre(nb int, pos [8]int) {
+	if nb == 8 {
+		for i := 0; i < 8; i++ {
+			z01.PrintRune(rune(pos[i] + 49))
+		}
+		z01.PrintRune(10)
+	} else {
+		for i := 0; i < 8; i++ {
+			if safe(nb, i, pos) {
+				pos[nb] = i
+				resoudre(nb+1, pos)
+			}
+		}
+	}
+}
 
 func EightQueens() {
-	print := true
-
-	for i := 0; i < 8; i++ {
-		for j := i + 1; j < 8; j++ {
-			if i == j {
-				continue
-			}
-			if x[i] == x[j] {
-				print = false
-				break
-			}
-			var diff rune
-			if diff = x[i] - x[j]; x[i] < x[j] {
-				diff = x[j] - x[i]
-			}
-			if diff == rune(j-i) {
-				print = false
-				break
-			}
-		}
-		if !print {
-			break
-		}
-	}
-
-	if print {
-		for _, r := range x {
-			z01.PrintRune(r)
-		}
-		z01.PrintRune('\n')
-	}
-
-	i := 7
-	for i > 0 && x[i-1] >= x[i] {
-		i--
-	}
-
-	if i <= 0 {
-		return
-	}
-
-	j := 7
-
-	for x[j] <= x[i-1] {
-		j--
-	}
-
-	t := x[i-1]
-	x[i-1] = x[j]
-	x[j] = t
-
-	j = 7
-	for i < j {
-		t := x[i]
-		x[i] = x[j]
-		x[j] = t
-		i++
-		j--
-	}
-	EightQueens()
+	pos := [8]int{0, 0, 0, 0, 0, 0, 0}
+	resoudre(0, pos)
 }
