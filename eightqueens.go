ec452237470d62447be463dd74092ddffdb30519
daiba <badaibou5@gmail.com>
2022-11-10 04:53:10 +0000

    my 42 go commit

--- eightqueens.go
+++ eightqueens.go
@@ -1,36 +1,38 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
-func safe(baye int, fall int, pos [8]int) bool {
-	for i := 0; i < baye; i++ {
-		t := pos[i]
-		if t == fall || t == fall-(baye-i) || t == fall+(baye-i) {
+const N = 8
+
+var position = [N]int{}
+
+func isSafe(queen_number, row_position int) bool {
+	for i := 0; i < queen_number; i++ {
+		other_row_pos := position[i]
+
+		if other_row_pos == row_position || other_row_pos == row_position-(queen_number-i) || other_row_pos == row_position+(queen_number-i) {
 			return false
 		}
 	}
 	return true
 }
 
-func resoudre(nb int, pos [8]int) {
-	if nb == 8 {
-		for i := 0; i < 8; i++ {
-			z01.PrintRune(rune(pos[i] + 49))
+func solve(k int) {
+	if k == N {
+		for i := 0; i < N; i++ {
+			z01.PrintRune(rune(position[i] + '1'))
 		}
-		z01.PrintRune(10)
+		z01.PrintRune('\n')
 	} else {
-		for i := 0; i < 8; i++ {
-			if safe(nb, i, pos) {
-				pos[nb] = i
-				resoudre(nb+1, pos)
+		for i := 0; i < N; i++ {
+			if isSafe(k, i) {
+				position[k] = i
+				solve(k + 1)
 			}
 		}
 	}
 }
 
 func EightQueens() {
-	pos := [8]int{0, 0, 0, 0, 0, 0, 0}
-	resoudre(0, pos)
+	solve(0)
 }
