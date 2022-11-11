ac8c4bdb8e978dfc8e6f9fbb6a037c978a990560
daiba <badaibou5@gmail.com>
2022-11-11 22:01:42 +0000

    my 52th go commit

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -6,24 +6,45 @@ import (
 	"github.com/01-edu/z01"
 )
 
+func BasicAtoi(s string) int {
+	x := 0
+	for _, n := range s {
+		y := 0
+		for i := '1'; i <= n; i++ {
+			y++
+		}
+		x = x*10 + y
+	}
+	return x
+}
+
 func main() {
-	lennbr := 0
-	bil := false
-	li := 96
-	// argument := os.Args
-	for in, correspond := range os.Args {
-		lennbr = in
-		if correspond == "---upper" {
-			bil = true
+	arguments := os.Args
+	position := 1
+	flag := false
+	compteur := 0
+	for range arguments {
+		compteur++
+	}
+
+	if compteur >= 2 && arguments[1] == "--upper" {
+		position = 2
+		flag = true
+	}
+
+	for index, as := range arguments {
+		if index >= position {
+			num := BasicAtoi(as)
+			if num == -1 {
+				z01.PrintRune(' ')
+			} else {
+				if !flag {
+					z01.PrintRune(rune('a' + num - 1))
+				} else {
+					z01.PrintRune(rune('A' + num - 1))
+				}
+			}
 		}
 	}
-	if bil == true {
-		li = 64
-	}
-	for i := 1; i <= lennbr; i++ {
-		for _, correspond := range os.Args[i] {
-			z01.PrintRune(correspond + rune(li))
-		}
-		z01.PrintRune(10)
-	}
+	z01.PrintRune(10)
 }
