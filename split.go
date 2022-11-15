906be194775f32c56b4d62067d134df1d91f4105
daiba <badaibou5@gmail.com>
2022-11-15 23:49:52 +0000

    my 59 go commit

--- split.go
+++ split.go
@@ -1,7 +1,29 @@
 package piscine
 
-import "strings"
+func Split(s, sep string) []string {
+	var word string
+	countLetter := 0
+	tab := make([]string, 0)
 
-func Split(str, charset string) []string {
-	return strings.Split(str, charset)
+	for i := 0; i < len(s); i++ {
+		count := 0
+		for j := 0; j < len(sep); j++ {
+			if i+j < len(s) && sep[j] == s[i+j] {
+				count += 1
+			}
+		}
+		if count != len(sep) {
+			word += string(s[i])
+			countLetter += 1
+		} else {
+			tab = append(tab, word)
+			word = ""
+			i = i + len(sep) - 1
+			countLetter = 0
+		}
+		if i == len(s)-1 && countLetter > 0 {
+			tab = append(tab, word)
+		}
+	}
+	return tab
 }
