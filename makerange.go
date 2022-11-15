26c501a91210bceea441d8ac279e31825dba1119
daiba <badaibou5@gmail.com>
2022-11-15 20:30:19 +0000

    my 55 go commit

--- makerange.go
+++ makerange.go
@@ -1,11 +1,11 @@
 package piscine
 
 func MakeRange(min, max int) []int {
-	if max <= min {
+	if max-min <= 0 {
 		var answer []int
 		return answer
 	}
-	answer := make([]int, min, max)
+	answer := make([]int, max-min)
 	for i := 0; i < max-min; i++ {
 		answer[i] = i + min
 	}
