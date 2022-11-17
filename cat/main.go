805458120c84100a8c5427b57bd5030f6aafee78
daiba <badaibou5@gmail.com>
2022-11-17 19:19:27 +0000

    my 64 go commit

--- cat/main.go
+++ cat/main.go
@@ -1,31 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
-func main() {
-	if len(os.Args) == 1 {
-		return
-	} else {
-		os.Args = os.Args[1:]
+func printTextFile(readfile string) {
+	runeReadfile := []rune(readfile)
+	for i := 0; i < len(runeReadfile); i++ {
+		z01.PrintRune(runeReadfile[i])
+	}
+}
 
-		for _, v := range os.Args {
-			file, err := os.Open(v)
+func printError(args string) {
+	err := "ERROR: open "
+
+	for i := 0; i < len(args); i++ {
+		err += string(args[i])
+	}
+
+	err += ": no such file or directory"
+
+	runeError := []rune(err)
+
+	for j := 0; j < len(runeError); j++ {
+		z01.PrintRune(runeError[j])
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		for i := 1; i < len(os.Args); i++ {
+			readFile, err := ioutil.ReadFile(os.Args[i])
 			if err != nil {
-				fmt.Println(err)
-				return
-			} else {
-				data := make([]byte, 443)
-				file.Read(data)
-				if len(os.Args) == 1 {
-					fmt.Println(string(data))
-				} else {
-					fmt.Println(string(data), "\n")
-				}
-				file.Close()
+				printError(os.Args[i])
+				os.Exit(1)
 			}
+			printTextFile(string(readFile))
 		}
+	} else {
+		bytes, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return
+		}
+		printTextFile(string(bytes))
 	}
 }
