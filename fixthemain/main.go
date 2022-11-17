cf47a5f3c2630128ab5a5c18b27612052056f756
daiba <badaibou5@gmail.com>
2022-11-17 18:41:19 +0000

    my third hackaton commit

---
+++ fixthemain/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+func PrintStr(str string) {
+	arrayRune := []rune(str)
+	for _, s := range arrayRune {
+		fmt.Printf(string(s))
+	}
+	fmt.Println()
+}
+
+func CloseDoor(ptrDoor *Door) {
+	PrintStr("Door closing...")
+	ptrDoor.state = false
+}
+
+func IsDoorOpen(door *Door) bool {
+	PrintStr("Door is open ?")
+	if door.state {
+		return true
+	} else {
+		return false
+	}
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("Door is close ?")
+	if ptrDoor.state {
+		return false
+	} else {
+		return true
+	}
+}
+
+type Door struct {
+	state bool
+}
+
+func OpenDoor(door *Door) {
+	PrintStr("Door Opening...")
+	door.state = true
+}
+
+func main() {
+	const OPEN = true
+
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+	if door.state == OPEN {
+		CloseDoor(door)
+	}
+}
