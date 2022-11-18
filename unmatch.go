78eb4ac6bfc479816f024968be75674d2b4e8b79
daiba <badaibou5@gmail.com>
2022-11-18 06:27:45 +0000

    my sixth hackaton commit

--- unmatch.go
+++ unmatch.go
@@ -1,21 +1,16 @@
 package piscine
 
 func Unmatch(arr []int) int {
-	table := make([]int, len(arr))
-
-	for i := 0; i < len(arr); i++ {
-		compteur := 0
-		for j := 0; j < len(arr); j++ {
-			if arr[i] == arr[j] {
-				compteur++
+	for i, iVal := range arr {
+		count := 1
+		for j, jVal := range arr {
+			if iVal == jVal && i != j {
+				count++
 			}
 		}
-		table[i] = compteur
-	}
-	for i, v := range table {
-		if v%2 != 0 {
-			return arr[i]
+		if count%2 == 1 {
+			return iVal
 		}
 	}
-	return 0
+	return -1
 }
