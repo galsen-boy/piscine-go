7e87561f397228323b23db8ad363bb9faa28543a
daiba <badaibou5@gmail.com>
2022-11-18 00:26:28 +0000

    my fourth hackaton commit

--- compact.go
+++ compact.go
@@ -1,11 +1,16 @@
 package piscine
 
-func Compact(ptr *[]string, length int) int {
+func Compact(ptr *[]string) int {
+	daiba := *ptr
+
 	compteur := 0
+
 	for _, v := range *ptr {
 		if v != " " {
+			daiba[compteur] = v
 			compteur++
 		}
 	}
+	*ptr = daiba[0:compteur]
 	return compteur
 }
