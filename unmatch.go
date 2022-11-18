dae2c3925eaaf7667b616ba98028bf4340a5809e
daiba <badaibou5@gmail.com>
2022-11-18 06:25:07 +0000

    my sixth hackaton commit

---
+++ unmatch.go
@@ -0,0 +1,21 @@
+package piscine
+
+func Unmatch(arr []int) int {
+	table := make([]int, len(arr))
+
+	for i := 0; i < len(arr); i++ {
+		compteur := 0
+		for j := 0; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				compteur++
+			}
+		}
+		table[i] = compteur
+	}
+	for i, v := range table {
+		if v%2 != 0 {
+			return arr[i]
+		}
+	}
+	return 0
+}
