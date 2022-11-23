d900aaade6f25517cfce2777845c21fe520cdfbd
daiba <badaibou5@gmail.com>
2022-11-23 01:15:52 +0000

    my 92 go commit

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -4,31 +4,12 @@ func BTreeIsBinary(root *TreeNode) bool {
 	if root == nil || root.Right == nil || root.Left == nil {
 		return true
 	}
-	daiba := false
-	if root.Left.Data < root.Data && root.Right.Data > root.Data {
-		daiba = BTreeIsBinary(root.Right)
-		if daiba == true {
-			daiba = BTreeIsBinary(root.Left)
+	checker := false
+	if root.Left.Data > root.Data && root.Right.Data < root.Data {
+		checker = BTreeIsBinary(root.Right)
+		if checker == true {
+			checker = BTreeIsBinary(root.Left)
 		}
 	}
-	return daiba
-}
-
-package piscine
-
-func BTreeIsBinary(root *TreeNode) bool {
-	if root == nil {
-		return true
-	}
-	if root.Left != nil && root.Left.Data > root.Data {
-		return false
-	}
-	if root.Right != nil && root.Right.Data < root.Data {
-		return false
-	}
-	if !BTreeIsBinary(root.Left) || !BTreeIsBinary(root.Right) {
-		return false
-	}
-
-	return true
+	return checker
 }
